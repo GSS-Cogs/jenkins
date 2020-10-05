bb23c41a98a6a6d2982e5f29dcecf9f82bb2e748
Catalina <catalina.garcia.mail@gmail.com>
2020-10-05 10:54:30 +0100

    Stops autocreating subnetworks

--- modules/global/vpc.tf
+++ modules/global/vpc.tf
@@ -2,6 +2,6 @@ resource "google_compute_network" "vpc" {
   name        = "${var.env}-${var.project}"
   description = "VPC for Jenkins resources"
 
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
   project                 = var.project
 }
