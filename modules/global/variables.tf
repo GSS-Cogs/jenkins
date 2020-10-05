c7caee7f4b15633cf836519e79fe479abf81bcbd
Catalina <catalina.garcia.mail@gmail.com>
2020-10-05 12:08:52 +0100

    Adds router and subnets

--- modules/global/variables.tf
+++ modules/global/variables.tf
@@ -3,3 +3,31 @@ variable "project" {}
 variable "env" {}
 
 variable "region" {}
+
+variable "subnet_name" {
+  default = "nodes"
+}
+
+variable "k8s_subnetwork_nodes_cidr" {
+  default = "10.110.0.0/16"
+}
+
+variable "k8s_subnetwork_pods_alias_cidr" {
+  default = "10.120.0.0/16"
+}
+
+variable "k8s_subnetwork_services_alias_cidr" {
+  default = "10.130.0.0/16"
+}
+
+variable "k8s_master_cidr" {
+  default = "10.140.0.0/28"
+}
+
+variable "service_connection_cidr" {
+  default = ""
+}
+
+variable "service_connection_name" {
+  default = "service-connection"
+}
--- modules/global/vpc.tf
+++ modules/global/vpc.tf
@@ -1,7 +1,58 @@
-resource "google_compute_network" "vpc" {
-  name        = "${var.env}-${var.project}"
-  description = "VPC for Jenkins resources"
+resource "google_compute_network" "k8s" {
+  name        = "k8s"
+  description = "VPC for kubernetes cluster"
 
   auto_create_subnetworks = false
   project                 = var.project
+}
+
+
+resource "google_compute_router" "nat" {
+  name    = "nat"
+  region  = var.region
+  network = google_compute_network.k8s.name
+}
+
+resource "google_compute_router_nat" "nat" {
+  name   = "nat"
+  region = var.region
+
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.nat.self_link]
+  router                             = google_compute_router.nat.name
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+resource "google_compute_address" "nat" {
+  name   = "nat"
+  region = var.region
+
+  network_tier = "STANDARD"
+}
+
+resource "google_compute_subnetwork" "nodes" {
+  name                     = var.subnet_name
+  ip_cidr_range            = var.k8s_subnetwork_nodes_cidr
+  network                  = join("", google_compute_network.k8s[*].self_link)
+  region                   = var.region
+  private_ip_google_access = "1"
+
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = var.k8s_subnetwork_pods_alias_cidr
+  }
+
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = var.k8s_subnetwork_services_alias_cidr
+  }
+}
+
+resource "google_compute_global_address" "service-connection" {
+  address       = var.service_connection_cidr
+  name          = var.service_connection_name
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 20
+  network       = google_compute_network.k8s.name
 }
