0980aced04166e166361f3b767b5221f9965c646
Catalina <catalina.garcia.mail@gmail.com>
2020-10-20 16:26:26 +0100

    Removes unused variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -19,15 +19,3 @@ variable "k8s_subnetwork_pods_alias_cidr" {
 variable "k8s_subnetwork_services_alias_cidr" {
   default = "10.130.0.0/16"
 }
-
-variable "k8s_master_cidr" {
-  default = "10.140.0.0/28"
-}
-
-variable "service_connection_cidr" {
-  default = ""
-}
-
-variable "service_connection_name" {
-  default = "service-connection"
-}
